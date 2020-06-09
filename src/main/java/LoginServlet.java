f259d45dfe7df52cc7f3ccdaf10c9778d55bf2e0
Ry Sutton <ryancesutton@gmail.com>
2020-06-09 14:08:47 -0500

    complete profile and login functionality start on logout

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -9,6 +9,11 @@ import java.io.IOException;
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        if (request.getSession().getAttribute("user") != null) {
+            response.sendRedirect("/profile");
+            return;
+        }
+
         request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
 
